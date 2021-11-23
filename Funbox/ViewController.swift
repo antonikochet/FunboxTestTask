b99a90be6d7bf0514815f229ecacd96c8c5c8972
antonikochet <anton_8598@mail.ru>
2021-11-23 12:53:06 +0300

    Initial Commit

---
+++ Funbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Funbox
+//
+//  Created by Антон Кочетков on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
